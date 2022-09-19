278f17c0da766bf34a7842e001f7252ddc34abc7
makyn <marcosvinicius0401@hotmail.com>
2022-09-19 16:13:03 -0300

    ViaCep

---
+++ source/repos/ViaCepTeste/ViaCepTeste/Controllers/CepController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViaCepTeste.Interfaces;
+using ViaCepTeste.Models;
+
+namespace ViaCep.Controllers
+{
+    [Route("api/[controller]")]
+    public class CepController : Controller
+    {
+        private readonly IMemoryCache _cache;
+        private const string cep_key = "Cep";
+        private readonly ICepCacheService _cacheService;
+
+        public CepController(IMemoryCache cache, ICepCacheService cacheService)
+        {
+            _cache = cache;
+            _cacheService = cacheService;
+        }
+
+        [HttpPost("GetCep")]
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult> GetCep([FromBody] CepModel cep, List<CepResult> ceps)
+        {
+            if (_cache.TryGetValue(cep_key, out List<CepResult> cepsL))
+            {
+                var result = cepsL.FirstOrDefault(x => x.cep.Replace("-", "") == cep.Cep).Origem = "Cache";
+                if (result != null)
+                    return Ok(cepsL);
+            }
+            await _cacheService.SetCache(cep_key, _cache, cep, ceps);
+            ceps.FirstOrDefault(x => x.cep.Replace("-", "") == cep.Cep).Origem = "ViaCep";
+            return Ok(ceps);
+        }
+
+    }
+}
---
+++ source/repos/ViaCepTeste/ViaCepTeste/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ViaCepTeste.Data;
+using ViaCepTeste.Models;
+using ViaCepTeste.Services;
+
+namespace ViaCepTeste.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public LoginController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User user)
+        {
+            user = _context.Get(user.Usuario, user.Senha);
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos." });
+
+            var token = await TokenService.GenerateToken(user);
+            user.Senha = "";
+            user.Token = token;
+
+            return Ok(new
+            {
+                usuario = user.Usuario,
+                tokenDeAcesso = token
+            });
+        }
+    }
+}

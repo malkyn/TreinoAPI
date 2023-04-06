d9fb7e562537aefadc6aafddd67d3b7121b42aa6
vinir <46539805+malkyn@users.noreply.github.com>
2023-04-06 10:54:16 -0300

    Delete source/repos/InfoSolutionsAPI/InfoSolutionsSite directory

--- source/repos/InfoSolutionsAPI/InfoSolutionsSite/src/app/shared/infosolution-details.model.ts
+++
@@ -1,9 +0,0 @@
-export class InfosolutionDetails {
-    Id:number = 0;
-    Nome:string= '';
-    Idade:number= 0;
-    Usuario:string= '';
-    Senha:string= '';
-    Token:string='';
-    Role:string= '';
-}
